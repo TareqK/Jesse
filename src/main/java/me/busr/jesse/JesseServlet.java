1370a96d3d07046324763adbca1d0ae6308f4cdd
Tareq Kirresh <kirreshtareq@gmail.com>
2018-11-24 18:43:25 +0200

    Used CachedTheadPool instead of SchedueledThreadPool for sending events, Added OPTION operation to JesseServlet, Added init params for Keep-Alive interval. Updated README.md

--- src/main/java/me/busr/jesse/JesseServlet.java
+++ src/main/java/me/busr/jesse/JesseServlet.java
@@ -24,16 +24,22 @@ import javax.servlet.http.HttpServletResponse;
 public class JesseServlet extends HttpServlet {
 
     private static final Logger LOG = Logger.getLogger(JesseServlet.class.getName());
-    private static final ExecutorService EXECUTOR = Executors.newScheduledThreadPool(15);
+    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
 
-   private SseSessionManager manager = new DefaultSessionManager();
+    private SseSessionManager manager = new DefaultSessionManager();
     private String domain = "*";
     private boolean keepAlive = false;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         AsyncContext asyncContext = request.startAsync();
         HttpServletResponse asyncContextResponse = (HttpServletResponse) asyncContext.getResponse();
-        asyncContextResponse.addHeader("Access-Control-Allow-Origin", domain);
+
+        if (domain == null || domain.contains("*")) {
+            asyncContextResponse.addHeader("Access-Control-Allow-Origin", request.getServerName());
+        } else {
+            asyncContextResponse.addHeader("Access-Control-Allow-Origin", domain);
+        }
         asyncContextResponse.addHeader("Access-Control-Expose-Headers", "*");
         asyncContextResponse.addHeader("Access-Control-Allow-Credentials", "true");
         EXECUTOR.submit(() -> {
@@ -41,19 +47,50 @@ public class JesseServlet extends HttpServlet {
 
         });
     }
+    
+    
+    
+    @Override
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        AsyncContext asyncContext = request.startAsync();
+        HttpServletResponse asyncContextResponse = (HttpServletResponse) asyncContext.getResponse();
+
+        if (domain == null || domain.contains("*")) {
+            asyncContextResponse.addHeader("Access-Control-Allow-Origin", request.getServerName());
+        } else {
+            asyncContextResponse.addHeader("Access-Control-Allow-Origin", domain);
+        }
+        asyncContextResponse.addHeader("Access-Control-Expose-Headers", "*");
+        asyncContextResponse.addHeader("Access-Control-Allow-Credentials", "true");
+        asyncContextResponse.flushBuffer();
+        asyncContext.complete();
+    }
+    
+
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         String sessionManagerClassName = getServletConfig().getInitParameter("me.busr.jesse.session.manager");
         String domainInitParameter = getServletConfig().getInitParameter("me.busr.jesse.session.domains");
-        String keepAliveParameter = getServletConfig().getInitParameter("me.busr.jesse.session.keepalive");
+        String keepAliveParameter = getServletConfig().getInitParameter("me.busr.jesse.session.keepalive.enabled");
+        String keepAliveTimer = getServletConfig().getInitParameter("me.busr.jesse.session.keepalive.interval");
         if (domainInitParameter != null) {
             this.domain = domainInitParameter;
         }
-        if(keepAliveParameter!=null && keepAliveParameter.equals("true")){
+        if (keepAliveParameter != null && keepAliveParameter.equals("true")) {
+            if(keepAliveTimer!=null){
+                try{
+                    long interval = Long.parseLong(keepAliveTimer.trim());
+                    SseSessionKeepAlive.setInterval(interval);
+                    LOG.info("Set the Keep-Alive interval to ".concat(String.valueOf(interval)).concat(" seconds"));
+                }catch(Exception ex){
+                    LOG.severe(ex.getMessage());
+                    LOG.info("Defaulting to Keep-Alive interval of 120 seconds");
+                }
+            }
             SseSessionKeepAlive.start();
-            this.keepAlive=true;
+            this.keepAlive = true;
         }
         try {
             Class<?> sessionManagerClass = Class.forName(sessionManagerClassName);
@@ -64,5 +101,5 @@ public class JesseServlet extends HttpServlet {
             LOG.warning(ex.getMessage().concat(" defaulting to built in session manager"));
         }
     }
-    
+
 }
